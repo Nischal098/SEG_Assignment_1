9aa0cdc807b3d1aaca2798d317a4081eef927627
nnepa066 <nishchalnepal12@yahoo.com>
2018-09-30 02:46:12 -0400

    iterators added

---
+++ Part B/arraylist_iterator.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class arraylist_iterator{
+	public static void main(String[] args){
+		ArrayList<Integer> list = new ArrayList<Integer>(165000000);
+		Random rand = new Random();
+		double time = System.nanoTime();
+		long sum = 0;
+
+		for (long i = 0; i<165000000; i++) {
+			list.add(rand.nextInt(10));
+		}
+
+		Iterator<Integer> iterator = list.iterator();	
+		while (iterator.hasNext()) {
+			sum = sum + iterator.next();
+		}
+		time = System.nanoTime() - time;
+		System.out.println("Running for 165000000 executions:");
+		System.out.println("\nTime taken = " + time/1000000000);
+		System.out.println("Sum = " + sum);
+	}	
+}
---
+++ Part B/vector_iterator.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class vector_iterator {	
+	public static void main(String[] args){
+		Vector vec = new Vector(165000000);
+		Random rand = new Random();
+		double time = System.nanoTime();
+		int sum = 0;
+
+		for (int i = 0; i<165000000; i++){
+			vec.add((int)(rand.nextInt(10)));
+		}
+
+		Iterator<Integer> iterator = vec.iterator();
+		while (iterator.hasNext()) {
+			sum = sum + iterator.next();
+		}
+		time = System.nanoTime() - time;
+		System.out.println("Running for 165000000 executions:");
+		System.out.println("\nTime taken = " + time/1000000000);
+		System.out.println("Sum = " + sum);
+	}	
+}
